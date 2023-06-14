faf8204e2fcd11f720ae2d36d9319783bd77d822
xh321 <xiaohe321@outlook.com>
2023-06-15 01:12:53 +0800

    支持AVID

--- renderer.js
+++ renderer.js
@@ -1,7 +1,10 @@
 export function onLoad() {
   setTimeout(() => {
+    var avid = __wxConfig.appLaunchInfo.query.avid;
+    console.log("Avid", avid);
     var bvid = __wxConfig.appLaunchInfo.query.bvid;
     console.log("Bvid", bvid);
     if (bvid != null) window.bilibili_external.openInExternal(bvid);
+    else if (avid != null) window.bilibili_external.openInExternal("av" + avid);
   }, 1000);
 }
